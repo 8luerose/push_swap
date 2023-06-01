bc3a451765672bb5cdf863bb6b1d8c4d25621d7c
Taehyun Kwon <taehkwon@c4r10s4.42seoul.kr>
2023-06-01 20:47:09 +0900

    just material (operation, deque ETC)

--- deque_op.c
+++ deque_op.c
@@ -1,25 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   deque_op.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/31 18:18:12 by taehkwon          #+#    #+#             */
-/*   Updated: 2023/06/01 18:24:16 by taehkwon         ###   ########.fr       */
+/*   Created: 2023/06/01 18:17:07 by taehkwon          #+#    #+#             */
+/*   Updated: 2023/06/01 18:24:15 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int ac, char *av[])
-{
-	int		array_size;
-	int		*num_array;
-	t_info	*info;
-	t_node	*p;
-
-
-	if (ac < 2)
-		print_error(-1);
-}
+t_node
--- sa_sb_ss.c
+++ sa_sb_ss.c
@@ -1,25 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   sa_sb_ss.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/31 18:18:12 by taehkwon          #+#    #+#             */
-/*   Updated: 2023/06/01 18:24:16 by taehkwon         ###   ########.fr       */
+/*   Created: 2023/06/01 18:10:39 by taehkwon          #+#    #+#             */
+/*   Updated: 2023/06/01 18:24:17 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
-
-int	main(int ac, char *av[])
-{
-	int		array_size;
-	int		*num_array;
-	t_info	*info;
-	t_node	*p;
-
-
-	if (ac < 2)
-		print_error(-1);
-}
+void	sa(t_)
