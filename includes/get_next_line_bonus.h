a222c1dff466386d9356b75a73ebda87a163ec78
Taehyun Kwon <taehkwon@c1r4s2.42seoul.kr>
2023-06-25 18:39:53 +0900

    +get_next_line + _bonus.h in header file

--- includes/get_next_line_bonus.h
+++ includes/get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 20:00:22 by taehkwon          #+#    #+#             */
-/*   Updated: 2023/06/20 20:01:35 by taehkwon         ###   ########.fr       */
+/*   Updated: 2023/06/25 18:39:46 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
 # include <stdlib.h>
